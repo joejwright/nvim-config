d23015742c6c881548686aad6b18d492ef0d0972
Joe Wright <joe@noventech.com>
2023-12-18 09:28:10 -0600

    Initial commit.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("c", "ll", function()
+  return "/"
+  -- return vim.fn.expand("%:h") .. "/"
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>ew", ':e <C-R>=expand("%:p:h") . "/" <CR>')
+vim.keymap.set("n", "<leader>es", ':sp <C-R>=expand("%:p:h") . "/" <CR>')
+vim.keymap.set("n", "<leader>ev", ':vsp <C-R>=expand("%:p:h") . "/" <CR>')
+
+-- Keymaps for commenting
+vim.keymap.set("n", "cc", function()
+  return vim.v.count == 0 and "<Plug>(comment_toggle_linewise_current)" or "<Plug>(comment_toggle_linewise_count)"
+end, { expr = true })
+
+vim.keymap.set("x", "cc", "<Plug>(comment_toggle_linewise_visual)")
