570d509ba02ee4d7b741a9bc0c634817229d850c
Joe Wright <joe@noventech.com>
2023-12-24 10:28:23 -0600

    Add vim options.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,16 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- turn on line numbers
+vim.cmd("set number")
+
+-- no line wrapping
+vim.cmd("set nowrap")
+
+-- new split windows should be on the right or bottom
+vim.cmd("set splitright")
+vim.cmd("set splitbelow")
+
 vim.g.mapleader = " "
 
