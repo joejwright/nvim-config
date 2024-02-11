a420510bcd8f9a51a23527832dbbb6ccdce46189
Joe Wright <joe@noventech.com>
2024-02-11 22:07:54 +0000

    Working on ruby-lsp configuration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,8 @@ return {
         ensure_installed = {
           "ruby_ls",
           "lua_ls",
-          "stimulus_ls",
+          -- "tsserver",
+          -- "stimulus_ls",
         },
         automatic_installation = true,
       })
@@ -31,13 +32,19 @@ return {
     lazy = false,
     config = function()
       local lspconfig = require("lspconfig")
-      -- lspconfig.tsserver.setup({})
       lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+      -- lspconfig.ruby_ls.setup({
+      --   cmd = { "bash", "-c", "rvm 3.3.0@ruby-lsp do bundle exec ruby-lsp --gemfile ~/.ruby-lsp/Gemfile" },
+      --   -- cmd = { "BUNDLE_GEMFILE=~/.ruby-lsp/Gemfile", "rvm", "3.3.0@ruby-lsp", "do", "bundle", "exec", "ruby-lsp" }
+      --   -- cmd = { "~/.ruby-lsp/launch" }
+      -- })
+
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
