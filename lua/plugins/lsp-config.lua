950462fc4e49099b58539bed7227a6d1b92cb2ff
Joe Wright <joe@noventech.com>
2024-02-12 17:35:51 +0000

    Add snippet and LSP completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,51 +1,54 @@
 return {
 	{
-    "williamboman/mason.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim"
-    },
-    config = function()
+		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			-- import mason
+			local mason = require("mason")
 
-      -- import mason
-      local mason = require("mason")
+			-- import mason-lspconfig
+			local mason_lspconfig = require("mason-lspconfig")
 
-      -- import mason-lspconfig
-      local mason_lspconfig = require("mason-lspconfig")
+			mason.setup({})
 
-      mason.setup({
-      })
+			mason_lspconfig.setup({
 
-      mason_lspconfig.setup({
-        ensure_installed = {
-          "ruby_ls",
-          "lua_ls",
-          -- "tsserver",
-          -- "stimulus_ls",
-        },
-        automatic_installation = true,
-      })
-
-    end,
+				ensure_installed = {
+					"ruby_ls",
+					"lua_ls",
+					-- "tsserver",
+					-- "stimulus_ls",
+				},
+				automatic_installation = true,
+			})
+		end,
 	},
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({})
 
-      -- requires rvm use 3.3.0@ruby-lsp --create --ruby-version
-      -- bundle binstubs ruby-lsp
-      lspconfig.ruby_ls.setup({
-        cmd = { "bash", "-c", "rvm 3.3.0@ruby-lsp do ~/.ruby/ruby-lsp/bin/ruby-lsp" },
-      })
+			-- requires rvm use 3.3.0@ruby-lsp --create --ruby-version
+			-- bundle binstubs ruby-lsp
+			lspconfig.ruby_ls.setup({
+				cmd = { "bash", "-c", "rvm 3.3.0@ruby-lsp do ~/.ruby/ruby-lsp/bin/ruby-lsp" },
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
