7f058bdb74a96923c19a8e37a494a674ff11309c
Joe Wright <joe@noventech.com>
2025-02-17 20:00:21 -0600

    Start with Neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,7 @@
+-- Disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
@@ -10,5 +14,20 @@ return {
 		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+
+		-- Add the autocmd for directory handling
+		vim.api.nvim_create_autocmd({ "VimEnter" }, {
+			callback = function(data)
+				-- buffer is a directory
+				local directory = vim.fn.isdirectory(data.file) == 1
+
+				if directory then
+					-- change to the directory
+					vim.cmd.cd(data.file)
+					-- open the tree
+					require("neo-tree.command").execute({ toggle = true, dir = data.file })
+				end
+			end
+		})
 	end,
 }
